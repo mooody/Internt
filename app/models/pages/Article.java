a634f2c65edb512758855691aaac6273fa1263c8
Mikael <mikael.forsberg@weetech.se>
2014-04-14 12:51:14 +0200

    content = lob in pages

--- app/models/pages/Article.java
+++ app/models/pages/Article.java
@@ -8,6 +8,7 @@ import javax.persistence.Basic;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Transient;
@@ -23,6 +24,7 @@ import play.db.jpa.Model;
 public class Article extends Model{
 	public String title;
         @Basic(fetch=FetchType.LAZY)
+        @Lob
 	public String content;
         
 	public boolean menuitem=false;
